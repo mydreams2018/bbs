c1c5284ad67c1316cff76b724540610591fb7070
bigben <bigben@886.com>
2019-05-05 10:14:10 +0800

    base

--- src/main/resources/doc/permission.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `permission` (`permission_methods`, `descript`) VALUES ('cn.kungreat.bbs.controller.LoginController-test', '测试');
--- src/main/resources/doc/persistent_logins.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `persistent_logins` (`username`, `series`, `token`, `last_used`) VALUES ('qepau886', 'IZK4YJLk+aYfMGJ8FgQDEQ==', 'UxTTzLowuctHMy06KzjQow==', '2019-5-4 20:38:27');
