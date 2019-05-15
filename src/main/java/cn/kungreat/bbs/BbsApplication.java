2151c0a7dc4a186da5752ff2b2a2d6f37342bc7b
kun great <mydreams2018@outlook.com>
2019-05-15 17:40:35 +0800

    https

--- src/main/java/cn/kungreat/bbs/BbsApplication.java
+++ src/main/java/cn/kungreat/bbs/BbsApplication.java
@@ -1,8 +1,12 @@
 package cn.kungreat.bbs;
 
+import org.apache.catalina.connector.Connector;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan(basePackages = "cn.kungreat.bbs.mapper")
@@ -12,4 +16,25 @@ public class BbsApplication {
 		SpringApplication.run(BbsApplication.class, args);
 	}
 
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createConnector());
+		return tomcat;
+	}
+	@Bean
+	public Connector createConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		try {
+			connector.setScheme("http");
+			connector.setSecure(false);
+			connector.setPort(80);
+			connector.setRedirectPort(443);
+			return connector;
+		}
+		catch (Exception ex) {
+			throw new IllegalStateException("can't access keystore: [" + "keystore"
+					+ "] or truststore: [" + "keystore" + "]", ex);
+		}
+	}
 }
