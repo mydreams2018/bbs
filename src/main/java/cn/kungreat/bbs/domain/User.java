90b2185c15634c640989cbf76f14b2b1967a32bd
bigben <bigben@886.com>
2019-06-19 19:25:04 +0800

    密码长度限制

--- src/main/java/cn/kungreat/bbs/domain/User.java
+++ src/main/java/cn/kungreat/bbs/domain/User.java
@@ -35,13 +35,14 @@ public class User {
     private Integer accumulatePoints=0;
     public String validMessage(){
         StringBuilder builder = new StringBuilder();
-        if(StringUtils.isEmpty(account) || account.length() < 6){
+        if(StringUtils.isEmpty(account) || account.getBytes().length < 6){
             builder.append("用户名不能为空-不能小于6位数");
         }
-        if(StringUtils.isEmpty(password) || password.length() < 6){
+        if(StringUtils.isEmpty(password) || password.getBytes().length < 6){
             builder.append("密码不能为空-不能小于6位数");
         }
         byte[] bytes = account.getBytes();
+        byte[] ps = password.getBytes();
         for(int x =0;x<bytes.length;x++){
             if(bytes[x] > 47 && bytes[x] < 58
                     || bytes[x] > 96 && bytes[x] < 123){
@@ -51,8 +52,20 @@ public class User {
                 break;
             }
         }
+        for(int x =0;x < ps.length; x++){
+            if(ps[x] > 47 && ps[x] < 58
+                    || ps[x] > 96 && ps[x] < 123 || ps[x] > 64 && ps[x] < 91){
+
+            }else{
+                builder.append("密码只能是字母和数字");
+                break;
+            }
+        }
         if(bytes.length > 12){
-            builder.append("账户长度不能大过12");
+            builder.append("账户长度不能大过12位");
+        }
+        if(ps.length > 18){
+            builder.append("密码长度不能大过18位");
         }
         return builder.toString();
     }
